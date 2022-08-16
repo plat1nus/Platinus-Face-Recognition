4a5767294b0133b96ccdf7bad8dcdfffc1b9a61a
Konstantin <platonovk517@gmail.com>
2022-08-16 19:29:18 +0300

    added ability to recognize multiple faces

--- face_detector.py
+++ face_detector.py
@@ -6,17 +6,20 @@ class FaceDetector:
         self.version = version
         self.paths = []
         self.names = []
-        self.image = None
-        self.image_encoding = None
+        self.images = []
+        self.image_encodings = []
         self.mode = None
 
     def get_original_face(self):
         print(f"Platinus face recognition. version={self.version}")
-        self.paths.append(input("Введите путь до изображения с нужным лицом: "))
-        self.names.append(input("Введите имя: "))
+        n = int(input("Введите количество(ЧИСЛО) изображений с неповторяющимися лицами: "))
+        print("Вводите путь к изображению и имя человека на фото.")
+        for i in range(n):
+            self.paths.append(input("Введите путь до изображения с нужным лицом: "))
+            self.names.append(input("Введите имя: "))
         try:
-            self.image = face_recognition.load_image_file(self.paths[0])  # temporarily
-            self.image_encoding = face_recognition.face_encodings(self.image)[0]
+            self.images = [face_recognition.load_image_file(path) for path in paths]  # temporarily
+            self.image_encodings = face_recognition.face_encodings(self.image)[0]
             return self.image_encoding, self.names
         except FileNotFoundError:
-            raise FileNotFoundError("Incorrect path to image")
+            raise FileNotFoundError("Вы ввели несуществущий путь к изображению")
